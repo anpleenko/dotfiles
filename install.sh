be8226d6bbc88c5f89cfeed31adaf8845f43659b
Anpleenko Alexander <avanpleenko@gmail.com>
2021-08-02 07:59:21 +0000

    remove fonts

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cp -vf JetBrainsMono/*.ttf ~/.local/share/fonts
-cp -vf iosevka/*.ttf ~/.local/share/fonts
-cp -vf RootUI/*.ttf ~/.local/share/fonts
-cp -vf Inter/*.otf ~/.local/share/fonts
-cp -vf LiberationMono/*.otf ~/.local/share/fonts
+cp -vf fonts/JetBrainsMono/*.ttf ~/.local/share/fonts
+cp -vf fonts/iosevka/*.ttf ~/.local/share/fonts
+cp -vf fonts/RootUI/*.ttf ~/.local/share/fonts
+cp -vf fonts/Inter/*.otf ~/.local/share/fonts
+cp -vf fonts/LiberationMono/*.otf ~/.local/share/fonts
 
 fc-cache -fv
