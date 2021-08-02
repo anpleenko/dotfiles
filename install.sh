b1b7972c78c176e00bfc23441a969fb3b7d20a1b
Anpleenko Alexander <avanpleenko@gmail.com>
2021-08-02 09:38:02 +0300

    added install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp -vf JetBrainsMono/*.ttf ~/.local/share/fonts
+cp -vf iosevka/*.ttf ~/.local/share/fonts
+cp -vf RootUI/*.ttf ~/.local/share/fonts
+cp -vf Inter/*.ttf ~/.local/share/fonts
+cp -vf Inter/*.otf ~/.local/share/fonts
+
+fc-cache -fv
