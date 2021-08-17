a64516f2aefbe5c95f5a8aa5ce2d3acd9550c2dc
Alexander Anpleenko <avanpleenko@gmail.com>
2021-08-17 15:17:14 +0300

    Update install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-ZSH_CUSTOM="~/.oh-my-zsh/custom"
-
 sudo apt install -y zsh
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+yes | cp -vf .zshrc ~/
+yes | cp -vf .zsh_aliases ~/
+yes | cp -vf .zsh_work_aliases ~/
 
 rm -rf ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions --depth=1
 
 rm -rf ~/.oh-my-zsh/custom/themes/spaceship-prompt
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
-yes | cp -vf .zshrc ~/
-yes | cp -vf .zsh_aliases ~/
-yes | cp -vf .zsh_work_aliases ~/
+cd  ~/.oh-my-zsh/custom/themes
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git spaceship-prompt --depth=1
+ln -s spaceship-prompt/spaceship.zsh-theme spaceship.zsh-theme
