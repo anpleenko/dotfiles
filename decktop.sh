fb0a205db600e971619f9fbb06bed114b69e652d
Anpleenko Alexander <avanpleenko@gmail.com>
2021-08-17 09:39:06 +0300

    added yes params

--- decktop.sh
+++ decktop.sh
@@ -11,10 +11,10 @@ sudo apt install -y gnome-tweak-tool \
 
 ./install_sublime_products.sh
 
-cp -vf fonts/JetBrainsMono/*.ttf ~/.local/share/fonts
-cp -vf fonts/iosevka/*.ttf ~/.local/share/fonts
-cp -vf fonts/RootUI/*.ttf ~/.local/share/fonts
-cp -vf fonts/Inter/*.otf ~/.local/share/fonts
-cp -vf fonts/LiberationMono/*.otf ~/.local/share/fonts
+yes | cp -vf fonts/JetBrainsMono/*.ttf ~/.local/share/fonts
+yes | cp -vf fonts/iosevka/*.ttf ~/.local/share/fonts
+yes | cp -vf fonts/RootUI/*.ttf ~/.local/share/fonts
+yes | cp -vf fonts/Inter/*.otf ~/.local/share/fonts
+yes | cp -vf fonts/LiberationMono/*.otf ~/.local/share/fonts
 
 fc-cache -fv
