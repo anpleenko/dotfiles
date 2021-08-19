f8496261223ead42639803e44b9f90f3ad279f3a
Anpleenko Alexander <avanpleenko@gmail.com>
2021-08-19 15:59:14 +0300

    rename font

--- fonts/install.sh
+++ fonts/install.sh
@@ -3,7 +3,7 @@
 mkdir -pv ~/.local/share/fonts
 
 yes | cp -vf JetBrainsMono/*.ttf ~/.local/share/fonts
-yes | cp -vf iosevka/*.ttf ~/.local/share/fonts
+yes | cp -vf Iosevka/*.ttf ~/.local/share/fonts
 yes | cp -vf RootUI/*.ttf ~/.local/share/fonts
 yes | cp -vf Inter/*.otf ~/.local/share/fonts
 yes | cp -vf LiberationMono/*.otf ~/.local/share/fonts
