b2a449316112580a7c8826b00136ceaf208aa259
Anpleenko Alexander <avanpleenko@gmail.com>
2021-08-20 20:32:43 +0000

    fix spaceship theme instalation

--- install_zsh.sh
+++ install_zsh.sh
@@ -8,4 +8,5 @@ rm -rf ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions --depth=1
 
 rm -rf ~/.oh-my-zsh/custom/themes/spaceship
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git ~/.oh-my-zsh/custom/themes/spaceship --depth=1
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$(eval echo ~$USER)/.oh-my-zsh/custom/themes/spaceship-prompt" --depth=1
+ln -s "$(eval echo ~$USER)/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$(eval echo ~$USER)/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
