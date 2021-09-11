ef5a866db26a5f7096718d22da4d6606c40f4a49
Alexander Anpleenko <avanpleenko@gmail.com>
2021-09-11 22:56:37 +0300

    Update install_nodejs_16.sh

--- install_nodejs_16.sh
+++ install_nodejs_16.sh
@@ -4,7 +4,6 @@ curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 sudo npm install -g yarn
-sudo npm install -g npm@7
 
 npm config set init.author.name "Alexander Anpleenko"
 npm config set init.author.email "avanpleenko@gmail.com"
