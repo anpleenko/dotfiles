37ef24d296aa52e7e3562de8bddae7e5e6802c9c
Alexander Anpleenko <avanpleenko@gmail.com>
2021-09-26 19:31:52 +0300

    Update install_git.sh

--- install_git.sh
+++ install_git.sh
@@ -7,3 +7,11 @@ sudo apt install -y git
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt -y install git-lfs
 git lfs install
+
+
+# cd /tmp
+# mkdir git-lfs
+# cd git-lfs
+# wget -O git-lfs.tar.gz https://github.com/git-lfs/git-lfs/releases/download/v2.13.3/git-lfs-linux-amd64-v2.13.3.tar.gz
+# tar xvzf git-lfs.tar.gz
+# sudo ./install.sh
