e21d7189af4a6547c1a6ba3adc846d20461d44d8
Alexander Anpleenko <avanpleenko@gmail.com>
2021-09-26 19:50:16 +0300

    Update install_git.sh

--- install_git.sh
+++ install_git.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-cd /tmp
-mkdir git-lfs
-cd git-lfs
 wget -O git-lfs.tar.gz https://github.com/git-lfs/git-lfs/releases/download/v2.13.3/git-lfs-linux-amd64-v2.13.3.tar.gz
-tar xvzf git-lfs.tar.gz
-sudo ./install.sh
+tar xvzf git-lfs.tar.gz --one-top-level=git-lfs
+sudo ./git-lfs/install.sh
