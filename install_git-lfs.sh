fcb88d94d06d61a4730221c97e326098d905a11b
Alexander Anpleenko <avanpleenko@gmail.com>
2021-09-27 22:10:07 +0300

    Delete install_git-lfs.sh

--- install_git-lfs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget -O git-lfs.tar.gz https://github.com/git-lfs/git-lfs/releases/download/v2.13.3/git-lfs-linux-amd64-v2.13.3.tar.gz
-tar xvzf git-lfs.tar.gz --one-top-level=git-lfs
-sudo ./git-lfs/install.sh
