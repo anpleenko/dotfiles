b2a50ac600d5c6545961e73a96792a93d05b16e9
Alexander Anpleenko <avanpleenko@gmail.com>
2023-06-13 09:37:59 +0300

    add  imagemagick

--- desktop.sh
+++ desktop.sh
@@ -14,7 +14,8 @@ sudo apt install -y \
   tilix \
   vlc \
   python3-pip \
-  python3-nautilus
+  python3-nautilus \
+  imagemagick
 
 # работа с HEIC
 sudo apt install -y heif-gdk-pixbuf heif-thumbnailer libheif1:amd64 gimagereader gpicview converseen
