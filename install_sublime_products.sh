1462e1e68b0750672f1e66cdc505a241edd72fbb
Alexander Anpleenko <avanpleenko@gmail.com>
2023-07-06 19:33:32 +0300

    Update install_sublime_products.sh

--- install_sublime_products.sh
+++ install_sublime_products.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
+sudo apt install -y apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
-sudo apt-get update
-sudo apt-get install sublime-text sublime-merge
+sudo apt update -y && sudo apt install -y sublime-text sublime-merge
 
 rm -rf ~/.config/sublime-text-3
 cd ~/.config/
