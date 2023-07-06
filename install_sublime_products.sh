a87b5cd3dbe602140fcc4f15b5a7102429ecb611
Alexander Anpleenko <avanpleenko@gmail.com>
2023-07-06 19:37:36 +0300

    Update install_sublime_products.sh

--- install_sublime_products.sh
+++ install_sublime_products.sh
@@ -2,6 +2,7 @@
 
 # wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 
+# импорт ключа https://zalinux.ru/?p=5066
 curl -s https://download.sublimetext.com/sublimehq-pub.gpg | sudo gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/sublimehq-pub.gpg --import
 sudo chmod 644 /etc/apt/trusted.gpg.d/sublimehq-pub.gpg
 
