fcbee09bca3c1970383e135e3ed68a7badf5d25a
Alexander Anpleenko <avanpleenko@gmail.com>
2023-09-04 21:53:14 +0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -5,6 +5,6 @@ sudo apt install -y docker.io
 sudo systemctl enable --now docker
 sudo usermod -aG docker ${USER}
 
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.19.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.21.0/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
