2f6dc11ca218d519170d7b89347efbda5198a454
Alexander Anpleenko <avanpleenko@gmail.com>
2024-01-11 09:59:52 +0300

    Update ssh-key.sh

--- scripts/ssh-key.sh
+++ scripts/ssh-key.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # создаем ssh ключ
-ssh-keygen -q -t ed25519 -C "$(whoami)@$(hostname)" -N '' -f ~/.ssh/id_ed25519 <<<y 2>&1 >/dev/null
+ssh-keygen -q -t rsa -C "$(whoami)@$(hostname)" -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
