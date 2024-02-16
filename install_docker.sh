5179c7fb53814378b129b7902442f2d132a795da
Alexander Anpleenko <avanpleenko@gmail.com>
2024-02-16 11:11:11 +0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y docker.io
+curl -fsSL https://get.docker.com -o install-docker.sh && sudo sh install-docker.sh
 
 sudo systemctl enable --now docker
 sudo usermod -aG docker ${USER}
