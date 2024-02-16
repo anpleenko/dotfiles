d8db66dd05cf6abcefdc5f0a190513bce8a74102
Alexander Anpleenko <avanpleenko@gmail.com>
2024-02-16 13:48:35 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -2,17 +2,17 @@
 
 ./common.sh
 
-sudo apt install -y \
-  build-essential \
-  ca-certificates \
-  cmake \
-  dpkg-dev \
-  fonts-liberation \
-  gcc \
-  gconf-service \
-  gnupg2 \
-  lsb-release \
-  openssl \
-  xdg-utils 
+# sudo apt install -y \
+#   build-essential \
+#   ca-certificates \
+#   cmake \
+#   dpkg-dev \
+#   fonts-liberation \
+#   gcc \
+#   gconf-service \
+#   gnupg2 \
+#   lsb-release \
+#   openssl \
+#   xdg-utils 
 
 ./postinstall.sh
