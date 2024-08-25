48dfd4d3e003523579d1f0b46059d52587812618
Alexander Anpleenko <avanpleenko@gmail.com>
2024-08-25 16:28:18 +0300

    Update install_python_3.sh

--- install_python_3.sh
+++ install_python_3.sh
@@ -2,7 +2,7 @@
 
 sudo apt install -y python3-pip
 
-sudo ln -s /usr/bin/python3 /usr/local/bin/python
-sudo ln -s /usr/bin/pip3 /usr/local/bin/pip
+# sudo ln -s /usr/bin/python3 /usr/local/bin/python
+# sudo ln -s /usr/bin/pip3 /usr/local/bin/pip
 
 sudo apt install pipx
