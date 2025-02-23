4c8eb61bf3d7d708b05c64601bd4877cadb759b3
Анплеенко Александр <avanpleenko@gmail.com>
2025-02-23 15:38:28 +0300

    install ansible

--- install_python_3.sh
+++ install_python_3.sh
@@ -2,3 +2,6 @@
 
 sudo apt install -y --no-install-recommends python3 python3-pip
 sudo apt install -y python-is-python3 pipx
+
+# http://unfriendlygrinch.info/posts/effortless-ansible-installation/
+# pipx install --include-deps ansible
