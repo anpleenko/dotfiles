0d5822a52189db0d50725f3b3badaf998e55a72a
Анплеенко Александр <avanpleenko@gmail.com>
2025-02-23 15:45:49 +0300

    update allure script

--- install_allure.sh
+++ install_allure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/allure-framework/allure2/releases/download/2.14.0/allure-2.14.0.tgz
+wget https://github.com/allure-framework/allure2/releases/download/2.32.2/allure-2.32.2.tgz
 
-sudo tar -zxvf allure-2.14.0.tgz -C /opt/
-sudo ln -s /opt/allure-2.14.0/bin/allure /usr/bin/allure
+sudo tar -zxvf allure-2.32.2.tgz -C /opt/
+sudo ln -s /opt/allure-2.32.2/bin/allure /usr/bin/allure
