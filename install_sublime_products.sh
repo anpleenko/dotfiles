4ed1920c9912dc77d75aa900d528a816d70f6b0c
Alexander Anpleenko <avanpleenko@gmail.com>
2025-03-01 00:42:03 +0300

    Update install_sublime_products.sh

--- install_sublime_products.sh
+++ install_sublime_products.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-# wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-
-# импорт ключа https://zalinux.ru/?p=5066
-curl -s https://download.sublimetext.com/sublimehq-pub.gpg | sudo gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/sublimehq-pub.gpg --import
-sudo chmod 644 /etc/apt/trusted.gpg.d/sublimehq-pub.gpg
-
 sudo apt install -y apt-transport-https
-
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-
 sudo apt update -y && sudo apt install -y sublime-text sublime-merge
 
 rm -rf ~/.config/sublime-text-3
