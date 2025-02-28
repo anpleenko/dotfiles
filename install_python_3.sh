dbea3171674d540e45dca400ebf40eea432f0c59
Alexander Anpleenko <avanpleenko@gmail.com>
2025-02-28 19:03:45 +0300

    Update install_python_3.sh

--- install_python_3.sh
+++ install_python_3.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y --no-install-recommends python3 python3-pip
-sudo apt install -y python-is-python3 pipx
+sudo apt install -y --no-install-recommends python3 python-is-python3 pipx
 
 # http://unfriendlygrinch.info/posts/effortless-ansible-installation/
 # pipx install --include-deps ansible
